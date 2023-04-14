f5d7f5e48ebef110cac057a9994cdf9265e557b8
Sergey Sharov <s.sharov@stud.rksi.ru>
2023-04-14 16:14:51 +0300

    add Task4

--- src/main/java/HomeWork01.java
+++ src/main/java/HomeWork01.java
@@ -1,22 +1,20 @@
 
 public class HomeWork01 {
     public static void main(String[] args) {
-        Task1 task1Obj = new Task1();
-        task1Obj.msg();
+//        Task1 task1Obj = new Task1();
+//        task1Obj.msg();
+//
+//        Task2 task2Obj = new Task2();
+//        task2Obj.msg();
+//
+//        Task3 task3Obj = new Task3();
+//        task3Obj.msg();
 
-        Task2 task2Obj = new Task2();
-        task2Obj.msg();
-
-        Task3 task3Obj = new Task3();
-        task3Obj.msg();
+//        Task4 task4Obj = new Task4();
+//        task4Obj.msg();
 
 
 
-//        4. В консоли запросить имя пользователя. В зависимости от текущего времени, вывести приветствие вида:
-//        "Доброе утро, <Имя>!", если время от 05:00 до 11:59
-//        "Добрый день, <Имя>!", если время от 12:00 до 17:59;
-//        "Добрый вечер, <Имя>!", если время от 18:00 до 22:59;
-//        "Доброй ночи, <Имя>!", если время от 23:00 до 4:59
     }
 
 
---
+++ src/main/java/Task4.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+public class Task4 {
+
+    public void msg() {
+        //        4. В консоли запросить имя пользователя.
+        //        В зависимости от текущего времени, вывести приветствие вида:
+        //        "Доброе утро, <Имя>!", если время от 05:00 до 11:59
+        //        "Добрый день, <Имя>!", если время от 12:00 до 17:59;
+        //        "Добрый вечер, <Имя>!", если время от 18:00 до 22:59;
+        //        "Доброй ночи, <Имя>!", если время от 23:00 до 4:59
+        String userName = getName();
+        String currentTime = getCurrentTime();
+        int intervalDay = getIntervalDay(currentTime);
+        switch (intervalDay) {
+            case 1 -> {
+                System.out.printf("Доброе утро, %s!%n", userName);
+            }
+            case 2 -> {
+                System.out.printf("Добрый день, %s!%n", userName);
+            }
+            case 3 -> {
+                System.out.printf("Добрый вечер, %s!%n", userName);
+            }
+            case 4 -> {
+                System.out.printf("Доброй ночи, %s!%n", userName);
+            }
+        }
+
+    }
+
+    public String getName() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Как вас зовут?: ");
+        String name = sc.nextLine();
+        sc.close();
+        return name;
+    }
+
+    public String getCurrentTime(){
+        return DateTimeFormatter.ofPattern("HH:mm")
+                .format(LocalDateTime.now());
+    }
+
+    public int getIntervalDay(String time){
+        String[] lTime = time.split(":");
+        int hour = Integer.parseInt(lTime[0]);
+        if (hour >= 5 & hour <= 11)
+            return 1;
+        else if (hour >= 12 & hour <= 17)
+            return 2;
+        else if (hour >= 18 & hour <= 22)
+            return 3;
+        return 4;
+    }
+}
